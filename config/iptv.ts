827972e55e6c5d4b1da51173445346f2d6504d3b
vlesshans <vlesshans@hotmail.com>
2025-07-23 06:55:57 +0800

    Add files via upload

---
+++ config/iptv.ts
@@ -0,0 +1,31 @@
+
+export const iptvSources = [
+  {
+    name: "IPTV-ORG 全频道",
+    url: "https://iptv-org.github.io/iptv/index.m3u",
+  },
+  {
+    name: "IPTV-ORG 英语频道",
+    url: "https://iptv-org.github.io/iptv/languages/eng.m3u",
+  },
+  {
+    name: "Free-TV",
+    url: "https://raw.githubusercontent.com/Free-TV/IPTV/master/playlist.m3u8",
+  },
+  {
+    name: "Apsattv - LocalNow",
+    url: "https://www.apsattv.com/localnow.m3u",
+  },
+  {
+    name: "Apsattv - Xumo",
+    url: "https://www.apsattv.com/xumo.m3u",
+  },
+  {
+    name: "Apsattv - LG Channels",
+    url: "https://www.apsattv.com/lg.m3u",
+  },
+  {
+    name: "Apsattv - Fire TV",
+    url: "https://www.apsattv.com/firetv.m3u",
+  },
+];
